6b2b27cd6b58dee5d86b68cad1d6541d9e44ccb6
Lean Mendoza <leandro@decentraland.org>
2023-11-20 10:09:35 -0300

    refactor: multi scene mode (#1)
    * - refactor to multi scene mode
    - add ci check linter and format
    - add ci github pages
    
    * fix format

---
+++ src/main-scene/index.ts
@@ -0,0 +1,21 @@
+import { ReactEcsRenderer } from '@dcl/sdk/react-ecs'
+import { MainSceneUi } from './ui'
+import { InputAction, engine, executeTask, inputSystem } from '@dcl/sdk/ecs'
+
+async function setMainSceneUi(): Promise<void> {
+  ReactEcsRenderer.setUiRenderer(MainSceneUi)
+}
+
+executeTask(setMainSceneUi)
+
+engine.addSystem(() => {
+  // console.log(inputSystem.isPressed(InputAction.IA_PRIMARY), inputSystem.isPressed(InputAction.IA_SECONDARY))
+  if (
+    inputSystem.isPressed(InputAction.IA_PRIMARY) &&
+    inputSystem.isPressed(InputAction.IA_SECONDARY)
+  ) {
+    ReactEcsRenderer.setUiRenderer(MainSceneUi)
+  }
+})
+
+export function main(): void {}
